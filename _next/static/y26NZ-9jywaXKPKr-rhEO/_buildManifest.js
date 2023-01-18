ad110334620ac974083aa26414ca50b8baf04d3d
Redskull-127 <76103614+Redskull-127@users.noreply.github.com>
2023-01-18 12:50:46 +0000

    Deploying to gh-pages from @ gdscwow/website-2023@9cad7ea3e14fbe7496d1dbb0b784534d270cf275 🚀

--- _next/static/y26NZ-9jywaXKPKr-rhEO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-76df9967b2c87e7f.js","static/css/69f81e06845cd7ae.css","static/chunks/pages/index-9576e45d66bb400c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
